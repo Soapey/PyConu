1e2b8a9a5a57401167d66d6ab128e3e9ee85aafc
Grant Soper <granttravissoper@gmail.com>
2023-03-07 13:47:32 +1100

    -a

--- conu/ui/components/Notification.py
+++ conu/ui/components/Notification.py
@@ -3,9 +3,17 @@ import winotify
 
 class Notification(winotify.Notification):
     def __init__(self, title, message_list):
+
+        if not message_list:
+            message = str()
+        elif len(message_list) == 1:
+            message = message_list[0]
+        else:
+            message = "\n".join(f"• {message}" for message in message_list)
+
         super().__init__(
             app_id="Conu",
             title=title,
-            msg="\n".join(f"• {message}" for message in message_list),
+            msg=message,
             icon=str()
         )
