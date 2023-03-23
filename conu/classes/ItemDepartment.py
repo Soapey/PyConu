daa011567249162a9184b18ae3c0ab39ee59e701
Grant Soper <granttravissoper@gmail.com>
2023-03-24 10:27:04 +1100

    Convert ItemDepartment from dataclass back to regular class

--- conu/classes/ItemDepartment.py
+++ conu/classes/ItemDepartment.py
@@ -1,8 +1,5 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class ItemDepartment:
-    id: int
-    item_id: int
-    department_id: int
+    def __init__(self, id: int, item_id: int, department_id: int) -> None:
+        self.id = id
+        self.item_id = item_id
+        self.department_id = department_id
