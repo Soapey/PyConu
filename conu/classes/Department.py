7005744762b22f918dbc666317a4dca81d7c6ee4
Grant Soper <granttravissoper@gmail.com>
2023-03-24 10:11:16 +1100

    Convert Department class from dataclass back to regular class and add get_listingview_table_data class method

--- conu/classes/Department.py
+++ conu/classes/Department.py
@@ -1,7 +1,28 @@
-from dataclasses import dataclass
+from conu.db.SQLiteConnection import SQLiteConnection
 
 
-@dataclass
 class Department:
-    id: int
-    name: str
+    def __init__(self, id: int, name: str) -> None:
+        self.id = id
+        self.name = name
+
+    @classmethod
+    def get_listingview_table_data(cls, main_window) -> list[tuple]:
+
+        current_user = main_window.current_user
+
+        if not current_user:
+            return
+
+        with SQLiteConnection() as cur:
+            rows = cur.execute(
+                """
+                SELECT department.id, department.name
+                FROM department
+                JOIN userdepartment ON department.id = userdepartment.department_id
+                WHERE userdepartment.user_id = ?;
+                """,
+                (current_user.id,),
+            ).fetchall()
+
+        return rows
