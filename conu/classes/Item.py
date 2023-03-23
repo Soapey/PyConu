a3e1b2cf54c549f2c6b0e0bfb99f4ad39d2c8aa4
Grant Soper <granttravissoper@gmail.com>
2023-03-24 10:25:50 +1100

    Convert Item from dataclass back to regular class and add get_listingview_table_data class method

--- conu/classes/Item.py
+++ conu/classes/Item.py
@@ -1,11 +1,11 @@
-from dataclasses import dataclass
+from conu.db.SQLiteConnection import SQLiteConnection
 
 
-@dataclass
 class Item:
-    id: int
-    name: str
-    comments: str
+    def __init__(self, id: int, name: str, comments: str) -> None:
+        self.id = id
+        self.name = name
+        self.comments = comments
 
     @classmethod
     def convert_rows_to_instances(cls, rows):
@@ -18,3 +18,25 @@ class Item:
             )
             for row in rows
         }
+
+    @classmethod
+    def get_listingview_table_data(cls, main_window) -> list[tuple]:
+
+        current_user = main_window.current_user
+
+        if not current_user:
+            return
+
+        with SQLiteConnection() as cur:
+            rows = cur.execute(
+                """
+                SELECT item.id, item.name, item.comments
+                FROM item
+                JOIN itemdepartment ON item.id = itemdepartment.item_id
+                JOIN userdepartment ON itemdepartment.department_id = userdepartment.department_id
+                WHERE userdepartment.user_id = ?;
+                """,
+                (current_user.id,),
+            ).fetchall()
+
+        return rows
