600daa9cf32445202612cef7465efc5a49cd2e7e
Grant Soper <granttravissoper@gmail.com>
2023-03-24 10:19:38 +1100

    Convert Form class from dataclass back to regular class and add get_listingview_table_data class method

--- conu/classes/Form.py
+++ conu/classes/Form.py
@@ -1,8 +1,26 @@
-from dataclasses import dataclass
+from conu.db.SQLiteConnection import SQLiteConnection
 
 
-@dataclass
 class Form:
-    id: int
-    name: str
-    path: str
+    def __init__(self, id: int, name: str, path: str) -> None:
+        self.id = id
+        self.name = name
+        self.path = path
+
+    @classmethod
+    def get_listingview_table_data(cls, main_window) -> list[tuple]:
+
+        current_user = main_window.current_user
+
+        if not current_user:
+            return
+
+        with SQLiteConnection() as cur:
+            rows = cur.execute(
+                """
+                SELECT form.id, form.name, form.path
+                FROM form;
+                """
+            ).fetchall()
+
+        return rows
