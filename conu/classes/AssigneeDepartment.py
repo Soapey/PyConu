155a869d5f1028f10b79cb2ef7327ad00e59cc0d
Grant Soper <granttravissoper@gmail.com>
2023-03-24 10:07:19 +1100

    Convert AssigneeDepartment class from dataclass back to regular class

--- conu/classes/AssigneeDepartment.py
+++ conu/classes/AssigneeDepartment.py
@@ -1,8 +1,5 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class AssigneeDepartment:
-    id: int
-    assignee_id: int
-    department_id: int
+    def __init__(self, id: int, assignee_id: int, department_id: int) -> None:
+        self.id = id
+        self.assignee_id = assignee_id
+        self.department_id = department_id
