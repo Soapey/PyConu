00900c454cfd36e95a66c99886c4a6c317827608
Grant Soper <granttravissoper@gmail.com>
2023-03-24 10:05:17 +1100

    Convert Assignee class from dataclass back to regular class and add get_listingview_table_data class method

--- conu/classes/Assignee.py
+++ conu/classes/Assignee.py
@@ -1,14 +1,14 @@
-from dataclasses import dataclass
+from conu.db.SQLiteConnection import SQLiteConnection
 
 
-@dataclass
 class Assignee:
-    id: int
-    name: str
-    description: str
+    def __init__(self, id: int, name: str, description: str) -> None:
+        self.id = id
+        self.name = name
+        self.description = description
 
     @classmethod
-    def convert_rows_to_instances(cls, rows):
+    def convert_rows_to_instances(cls, rows) -> dict:
 
         return {
             row[0]: cls(
@@ -18,3 +18,25 @@ class Assignee:
             )
             for row in rows
         }
+
+    @classmethod
+    def get_listingview_table_data(cls, main_window) -> list[tuple]:
+
+        current_user = main_window.current_user
+
+        if not current_user:
+            return
+
+        with SQLiteConnection() as cur:
+            rows = cur.execute(
+                """
+                SELECT assignee.id, assignee.name, assignee.description
+                FROM assignee
+                JOIN assigneedepartment ON assignee.id = assigneedepartment.assignee_id
+                JOIN userdepartment ON assigneedepartment.department_id = userdepartment.department_id
+                WHERE userdepartment.user_id = ?;
+                """,
+                (current_user.id,),
+            ).fetchall()
+
+        return rows
