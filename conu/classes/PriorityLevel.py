e4a37b946bc7588a9daeda195968bed81545eaf2
Grant Soper <granttravissoper@gmail.com>
2023-03-24 10:29:34 +1100

    Convert PriorityLevel from dataclass back to regular class and add get_listingview_table_data class method

--- conu/classes/PriorityLevel.py
+++ conu/classes/PriorityLevel.py
@@ -1,8 +1,26 @@
-from dataclasses import dataclass
+from conu.db.SQLiteConnection import SQLiteConnection
 
 
-@dataclass
 class PriorityLevel:
-    id: int
-    name: str
-    days_until_overdue: int
+    def __init__(self, id: int, name: str, days_until_overdue: int) -> None:
+        self.id = id
+        self.name = name
+        self.days_until_overdue = days_until_overdue
+
+    @classmethod
+    def get_listingview_table_data(cls, main_window) -> list[tuple]:
+
+        current_user = main_window.current_user
+
+        if not current_user:
+            return
+
+        with SQLiteConnection() as cur:
+            rows = cur.execute(
+                """
+                SELECT prioritylevel.id, prioritylevel.name, prioritylevel.days_until_overdue
+                FROM prioritylevel;
+                """
+            ).fetchall()
+
+        return rows
