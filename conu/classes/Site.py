16cf3ac6958e795373b6106f89d54f72e5cea30e
Grant Soper <granttravissoper@gmail.com>
2023-03-24 10:58:25 +1100

    Convert Site dataclass back to regular class and add get_listingview_table_data class method

--- conu/classes/Site.py
+++ conu/classes/Site.py
@@ -1,9 +1,29 @@
-from dataclasses import dataclass
+from conu.db.SQLiteConnection import SQLiteConnection
 
 
-@dataclass
 class Site:
-    id: int
-    name: str
-    address: str
-    suburb: str
+    def __init__(
+        self, id: int = None, name: str = None, address: str = None, suburb: str = None
+    ) -> None:
+        self.id = id
+        self.name = name
+        self.address = address
+        self.suburb = suburb
+
+    @classmethod
+    def get_listingview_table_data(cls, main_window) -> list[tuple]:
+
+        current_user = main_window.current_user
+
+        if not current_user:
+            return
+
+        with SQLiteConnection() as cur:
+            rows = cur.execute(
+                """
+                SELECT site.id, site.name, site.address, site.suburb
+                FROM site;
+                """
+            ).fetchall()
+
+        return rows
