bbdc786ddbc3be6d5e07e3298e58ad926d124ec0
Grant Soper <granttravissoper@gmail.com>
2023-03-24 12:39:07 +1100

    Convert WorkOrderItem dataclass back to regular class

--- conu/classes/WorkOrderItem.py
+++ conu/classes/WorkOrderItem.py
@@ -1,11 +1,10 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class WorkOrderItem:
-    id: int
-    workorder_id: int
-    item_id: int
+    def __init__(
+        self, id: int = None, workorder_id: int = None, item_id: int = None
+    ) -> None:
+        self.id = id
+        self.workorder_id = workorder_id
+        self.item_id = item_id
 
     def __eq__(self, __o: object) -> bool:
         return self.id == __o.id
